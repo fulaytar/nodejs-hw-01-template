a7ea7551691d81349fa69588e57792f7b3330d0c
fulaytar <fulaytar2405@gmail.com>
2024-06-07 13:36:17 +0300

    base

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,10 @@
-export const PATH_DB =
+import path from 'node:path';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'node:fs';
+const main = path.join(process.cwd());
+export const PATH_DB = path.join(main, 'src/db', 'db.json');
+const read = fs.readFileSync(PATH_DB);
+const data = createFakeContact();
+
+//console.log(data);
+//console.log(read.toString());
