ccc9c790829b45f86f73304a75ec4019588d9ead
fulaytar <fulaytar2405@gmail.com>
2024-06-14 15:22:27 +0300

    Revert "Revert "base""
    This reverts commit bf96db95cf5e416d472102493184253a63bb4270.

--- src/constants/contacts.js
+++
@@ -1,8 +0,0 @@
-import path from 'node:path';
-
-export const PATH_DB = path.resolve('src/db', 'db.json');
-
-//визначення кореня папки або через path.resolve()
-
-//ComonJS   __dirname
-//ES6      process.cwd()
